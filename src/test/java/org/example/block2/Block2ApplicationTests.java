e00e1a42f4807f9c758cea1bd98cc190071279c5
Keyasarin <umanskaya.katia12@gmail.com>
2026-08-28 11:54:09 +0100

    Initial commit

---
+++ src/test/java/org/example/block2/Block2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.block2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
