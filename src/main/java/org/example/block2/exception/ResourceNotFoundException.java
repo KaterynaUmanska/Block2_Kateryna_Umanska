e501008811525835de432bd7fa7ac182cf3901c2
Keyasarin <umanskaya.katia12@gmail.com>
2026-09-09 20:20:27 +0100

    Initial commit

--- src/main/java/org/example/block2/exception/ResourceNotFoundException.java
+++ src/main/java/org/example/block2/exception/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package org.example.block2.exception;
 
+/**
+ * Exception thrown when resource not found.
+ */
 public class ResourceNotFoundException  extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
